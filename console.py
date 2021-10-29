2c32fc4d5fb747c6c1253b509a4f724de5096df1
RockoGilbert <evolve2122@gmail.com>
2021-10-29 18:09:45 -0500

    Added console for interaction

--- console.py
+++ console.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Console for AirBnB project"""
+import cmd
+import models
+import readline
+
+
+class HBNBCommand(cmd.Cmd):
+    """Controls the HBNB terminal """
+    prompt = "(hbnb)"
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
