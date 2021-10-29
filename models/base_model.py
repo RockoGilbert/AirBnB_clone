cfc1d96ed8f6bff5dbf4cd8ea9f1f82d71403413
RockoGilbert <evolve2122@gmail.com>
2021-10-29 09:22:29 -0500

    Added Base model to be the source of all future instance

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""The base class for models"""
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel():
+    """Base model from which future instances are derived"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializing of the base model"""
+
+        if kwargs:
+            for key, value in kwargs.items()
+
+                if key == "created at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                self.__dict__[key] = value
+
+            else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the BaseModel Class"""
+        
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """Updates the attribute 'updated_at' with the current datetime"""
+        
+        self.updated_at = datetime.utcnow()
+        model.storage.save()
+    
+    def to_dict(self):
+        """Method to return the dictionary representation of our object."""
+        
+        x_dict = self.__dict__copy()
+        x_dict = ['__class__'] = self.__class__.__name__
+        x_dict = ['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        x_dict = ['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return x_dict
