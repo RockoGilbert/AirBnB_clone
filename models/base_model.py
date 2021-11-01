04b7e82c8a478bebffe3fc856898cb23b526e128
RockoGilbert <evolve2122@gmail.com>
2021-11-01 11:40:59 -0500

    test

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 import models
 import uuid
 from datetime import datetime
-import time
 
 
 class BaseModel():
@@ -20,8 +19,8 @@ class BaseModel():
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 self.__dict__[key] = value
 
-            else:
-                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
